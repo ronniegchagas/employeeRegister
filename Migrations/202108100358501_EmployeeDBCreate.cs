6763327f93efeb8cd322d83d7fdef5934003e24e
Ronnie G. Chagas <dev.ronniegchagas@gmail.com>
2021-08-10 01:09:27 -0300

    Add DBContext and Migration

---
+++ Migrations/202108100358501_EmployeeDBCreate.cs
@@ -0,0 +1,29 @@
+﻿namespace employeeRegister.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EmployeeDBCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employee",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Address = c.String(),
+                        BirthDate = c.String(),
+                        Salary = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employee");
+        }
+    }
+}
---
+++ Models/EmployeeContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace employeeRegister.Models
+{
+    public class EmployeeContext : DbContext
+    {
+
+        public EmployeeContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
