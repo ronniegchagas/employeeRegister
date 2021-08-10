1ad8c269f034332c424d675031ee265716340eb0
Ronnie G. Chagas <dev.ronniegchagas@gmail.com>
2021-08-10 00:39:16 -0300

    Add Model Employee and Connection String

---
+++ Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace employeeRegister.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+
+        [Display(Name = "Endereço")]
+        public string Address { get; set; }
+
+        [Display(Name = "Data Nascimento")]
+        public string BirthDate { get; set; }
+
+        [Display(Name = "Salário (R$)")]
+        public string Salary { get; set; }
+    }
+}
